ec34356f511025280402e10923dd447b1eeb6aa5
slava <svjat68@mail.ru>
2026-09-22 23:35:08 +0300

    Files button.[hc] are added into project.

---
+++ Timer/button.c
@@ -0,0 +1,23 @@
+#include "button.h"
+
+void init_button(){
+}
+
+void probe_click(){
+}
+
+void probe_double(){
+}
+
+void probe_long(){
+}
+
+void click(){
+}
+
+void doubleclick(){
+}
+
+void longdown(){
+}
+
---
+++ Timer/button.h
@@ -0,0 +1,12 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+/*
+*/
+
+void init_button();
+void click();
+void doubleclick();
+void longdown();
+
+#endif // BUTTON_H
+
